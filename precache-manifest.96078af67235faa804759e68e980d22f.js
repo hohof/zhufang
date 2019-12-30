a76fac708004bcab49f632836ef8eeea56bb9ba5
Tuuu <tutengfei1994@gmail.com>
2019-12-30 20:28:51 +0800

    Updates

---
+++ precache-manifest.96078af67235faa804759e68e980d22f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276845173a4001e9599240f77e57276e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2886b1fcbb8bc5507510",
+    "url": "/static/css/main.b2084220.chunk.css"
+  },
+  {
+    "revision": "bcfb9c019d5d5540fa20",
+    "url": "/static/js/2.7e15b080.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7e15b080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2886b1fcbb8bc5507510",
+    "url": "/static/js/main.82f2f9be.chunk.js"
+  },
+  {
+    "revision": "7df52723d36a62aa5396",
+    "url": "/static/js/runtime-main.3127cda7.js"
+  },
+  {
+    "revision": "23f6f5f03cd460d0e1b5c764d972222e",
+    "url": "/static/media/bg.23f6f5f0.png"
+  },
+  {
+    "revision": "8d3b342009ef6e86febcd9ffb478e397",
+    "url": "/static/media/header.8d3b3420.png"
+  }
+]);
--- src/pages/home/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './Header.module.scss'
-
-function Home() {
-  return <header className={styles.header} />
-}
-
-export default Home
